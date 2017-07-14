62b7a38f836355f39d4444225674923e23dc10cf
Marcelo Siqueira <marcelo@cyb3r5cape.home>
2017-07-14 08:45:37 -0300

    Initial Commit

---
+++ iClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iClock
+//
+//  Created by Marcelo Siqueira on 14/07/17.
+//  Copyright © 2017 Marcelo Siqueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
